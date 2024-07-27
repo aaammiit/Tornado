a0944326b37101b4ea47e8862b2ecbaeac4b3b71
Amit Kumar Dwivedi <159676340+aaammiit@users.noreply.github.com>
2024-07-27 11:13:11 +0530

    Update settings.py

--- Krima_project/settings.py
+++ Krima_project/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
    
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
