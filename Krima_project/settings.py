1d75d7fb88679fea454583a0d60381990d617433
Amit Kumar Dwivedi <159676340+aaammiit@users.noreply.github.com>
2024-07-27 11:03:35 +0530

    Update settings.py

--- Krima_project/settings.py
+++ Krima_project/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
 STATIC_URL = 'static/'
 
@@ -124,7 +124,8 @@ STATICFILES_DIRS = [
    
 ]
 STATIC_ROOT = "static/"
-STATICFILES_DIRS=BASE_DIR,'/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
