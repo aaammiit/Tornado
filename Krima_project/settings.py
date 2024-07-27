c61e55fd8d8eaa0889077cf91889b222d044201b
Amit Kumar Dwivedi <159676340+aaammiit@users.noreply.github.com>
2024-07-27 11:08:06 +0530

    Update settings.py

--- Krima_project/settings.py
+++ Krima_project/settings.py
@@ -123,8 +123,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
    
 ]
-STATIC_ROOT = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
